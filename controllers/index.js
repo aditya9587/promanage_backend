6b2e03d1c478366065ffcfe701a8679f9afb6583
Aditya rai <135973480+aditya9587@users.noreply.github.com>
2024-10-27 22:43:25 +0530

    add routes

--- controllers/index.js
+++ controllers/index.js
@@ -24,77 +24,101 @@ export const createUser = async (req, res) => {
   }
 };
 
-export const loginUser = async (req,res)=>{
+export const loginUser = async (req, res) => {
   try {
-    const{ email,password } = req.body;
-    const userCheck = await userData.findOne({email})
-    if(!userCheck){
-      return res.status(400).json({message:"user not exist"})
+    const { email, password } = req.body;
+    const userCheck = await userData.findOne({ email });
+    if (!userCheck) {
+      return res.status(400).json({ message: "user not exist" });
     }
-    const verifyPassword = await bcrypt.compare(password, userCheck.password)
-    if(!verifyPassword){
-      return res.status(400).json({message:" invalid credtinals "})
+    const verifyPassword = await bcrypt.compare(password, userCheck.password);
+    if (!verifyPassword) {
+      return res.status(400).json({ message: " invalid credtinals " });
     }
-    const payload = {key : userCheck._id }
-    const token = jwt.sign(payload, process.env.SECRETKEY)
-    return res.status(200).json({message:"user login successfull", Token: token})
+    const payload = { key: userCheck._id };
+    const token = jwt.sign(payload, process.env.SECRETKEY);
+    return res
+      .status(200)
+      .json({ message: "user login successfull", Token: token });
   } catch (error) {
     return res.status(500).json({ message: "Server error" });
   }
-}
+};
 
-export const todoCreate = async(req,res) =>{
+export const todoCreate = async (req, res) => {
   try {
-    const{title, priority , checklist , dueDate} = req.body;
+    const { title, priority, checklist, dueDate } = req.body;
     const { user } = req;
-    const data = new todoData({title,priority,checklist,dueDate, taskID : user })
-    console.log(data)
+    const data = new todoData({
+      title,
+      priority,
+      checklist,
+      dueDate,
+      taskID: user,
+    });
+    console.log(data);
     await data.save();
-    return res.status(200).json({message : "todo created Sucessfully",datamsg : data})
- 
+    return res
+      .status(200)
+      .json({ message: "todo created Sucessfully", datamsg: data });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json({ message: "Server error" });
   }
+};
 
-}
-
-export const getTodos =  async (req,res) =>{
+export const getTodos = async (req, res) => {
   try {
-    const data  = await todoData.find({ taskID: req.user });
-    return res.status(200).json({ data})
+    const data = await todoData.find({ taskID: req.user });
+    return res.status(200).json({ data });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-export const updateTodo = async (req, res) =>{
+export const updateTodo = async (req, res) => {
   const { id } = req.params;
-  const {status} = req.body
+  const { status } = req.body;
   if (!id || !status) {
-    return res.status(400).json({ message: "Invalid request. Task ID and status are required." });
+    return res
+      .status(400)
+      .json({ message: "Invalid request. Task ID and status are required." });
   }
   try {
-    const updatedTask = await todoData.findByIdAndUpdate(id, {status },{new:true})
-    res.status(200).json({ data: updatedTask });
+    const updatedTask = await todoData.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+    return res.status(200).json({ data: updatedTask });
   } catch (error) {
-    res.status(500).json({ message: 'Error updating task status', error });
+    return res
+      .status(500)
+      .json({ message: "Error updating task status", error });
   }
-}
+};
 
-export const deleteTask = async (req,res) =>{
+export const deleteTask = async (req, res) => {
   const { id } = req.params;
   try {
     const deleteTask = await todoData.findByIdAndDelete(id);
-    res.status(200).json({deleteTask})
+    return res.status(200).json({ message: " Todo deleted sucessfully" });
   } catch (error) {
-    res.status(500).json({message: "Error deleting task Status"})
+    return res.status(500).json({ message: "Error deleting task Status" });
   }
-}
+};
 
-export const updateEmail = async (req, res) =>{
+export const editTask = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const editTask = await todoData.findByIdAndUpdate(id);
+    return res.status(200).json({ message: "TO do updated Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error deleting task Status" });
+  }
+};
+
+export const updateEmail = async (req, res) => {
   const { email } = req.params;
-  const update = await userData.find(email)
-  
-
-}
+  const update = await userData.find(email);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser, loginUser,todoCreate , getTodos, updateTodo, deleteTask} from '../controllers/index.js';
+import { createUser, loginUser,todoCreate , getTodos, updateTodo, deleteTask, editTask} from '../controllers/index.js';
 import { AuthMiddleware } from '../Middleware/Auth.js';
 
 export const userRouter = express.Router();
@@ -15,3 +15,5 @@ userRouter.get("/tasks/" , AuthMiddleware ,getTodos )
 userRouter.patch("/:id/status",AuthMiddleware ,updateTodo)
 
 userRouter.delete("/deleteTask/:id",AuthMiddleware, deleteTask)
+
+userRouter.patch("/editTask/:id",AuthMiddleware,editTask)
